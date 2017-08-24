0447c07910491b1a6dfedfe868536a6dcf5f3a2d
MAKOSA <makosagitsit@anomanode.com>
2017-08-24 17:10:35 -0400

    Removal of Previous Project to Reinstate it.

--- app/src/main/java/com/marionthefourth/augimas/classes/objects/FirebaseContent.java
+++
@@ -1,8 +0,0 @@
-package com.marionthefourth.augimas.classes.objects;
-
-public abstract class FirebaseContent extends FirebaseObject {
-    private String teamUID;
-//    Class Getters & Setters
-    public final String getTeamUID() { return teamUID; }
-    public final void setTeamUID(final String teamUID) { this.teamUID = teamUID; }
-}
